6697c3e9a0f6505f258a8e82b31bbe13bc619a46
Timon Loeffen <tloeffen@bethel.jw.org>
2018-05-04 11:49:49 +0200

    extract store to module

--- script.js
+++ script.js
@@ -1,78 +1,9 @@
-const MINUTE = 60 * 1000
+import store from './store.js'
 
 const util = {
-  msToMin: ms => Math.floor(ms / MINUTE),
   load: () => JSON.parse(window.localStorage.getItem('tasks') || '[]'),
   save: (tasks) => window.localStorage.setItem('tasks', JSON.stringify(tasks))
 }
-
-const store = new Vuex.Store({
-  state: {
-    tasks: [],
-    current: null
-  },
-  actions: {
-    tick: function ({ commit, state, dispatch }) {
-      window.setTimeout(() => {
-        if (state.current) {
-          commit('updateTime')
-          dispatch('tick')
-        }
-      }, MINUTE)
-    }
-  },
-  mutations: {
-    load: function (state, tasks) {
-      state.tasks = tasks
-      const active = state.tasks.filter(t => t.active)
-      if (active.length > 0) {
-        this.commit('changeTask', active[0].text)
-      }
-    },
-    updateTime: function (state) {
-      const task = state.tasks.find(x => x.text === state.current.text),
-        now = Date.now(),
-        elapsed = now - state.current.started
-      task.time += util.msToMin(elapsed)
-      state.current.started = now
-    },
-    changeTask: function (state, task) {
-      const store = this
-      function activate (newCurrent) {
-        newCurrent.active = true
-        state.current = {
-          text: task,
-          started: now
-        }
-        store.dispatch('tick')
-      }
-      let now = Date.now()
-      let newCurrent = state.tasks.find(x => x.text === task)
-      if (state.current) {
-        var prev = state.tasks.find(x => x.text === state.current.text)
-        prev.time += util.msToMin(now - state.current.started)
-        prev.active = false
-        if (state.current.text === newCurrent.text) {
-          state.current = null
-        } else {
-          activate(newCurrent)
-        }
-      } else {
-        activate(newCurrent)
-      }
-      document.title = 'Tasks - ' + ((state.current && state.current.text) || 'Not working on anything')
-      util.save(state.tasks)
-    },
-    newTask: function (state, task) {
-      state.tasks.push({
-        text: task,
-        time: 0,
-        active: false
-      })
-      util.save(state.tasks)
-    }
-  }
-})
 
 Vue.component('Tasks', {
   methods: {
@@ -147,5 +78,4 @@ var app = new Vue({
     el: '#app',
     store
 })
-
 
